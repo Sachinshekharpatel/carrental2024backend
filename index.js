6e94ce7c000abff5d34cabdb2db680846c045fbd
Sachinshekharpatel <heroft7024@gmail.com>
2024-05-23 15:22:18 +0530

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const app = express();
 const cors = require("cors");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
  
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
